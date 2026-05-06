909794e178b438f97d22bf68d16f26cc16b7b1ec
Santi <mazzullasantino85@gmail.com>
2026-05-06 10:48:37 -0300

    feat: se agregan validaciones al boton listener

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@ const input = document.getElementById('inputTarea');
 const unorderedList = document.getElementById('unorderedListTareas');
 
 boton.addEventListener('click', function(){
+    if(input.value.trim() === ""){
+        return
+    }
     const list = document.createElement('li');
     list.textContent = input.value;
     unorderedList.appendChild(list);
